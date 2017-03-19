cff7bac4705eba623e05af39f167811ee372bd68
Teun Huibregtse <huib0029@hz.nl>
2017-03-19 17:00:54 +0100

    Typo blibliothecaris

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -57,7 +57,7 @@
         </div>
 
         <ul class="nav navbar-top-links navbar-right">
-            <li><a href="{{ url('/login') }}"><i class="fa fa-university fa-2x fa-fw" aria-hidden="true"></i>&nbsp;Biblioticaris / Bibliothecaresse login</a></li>
+            <li><a href="{{ url('/login') }}"><i class="fa fa-university fa-2x fa-fw" aria-hidden="true"></i>&nbsp;Bibliothecaris / Bibliothecaresse login</a></li>
             <li><a href="{{ url('/register') }}">Registreren (admin)</a></li>
         </ul>
     </nav>
