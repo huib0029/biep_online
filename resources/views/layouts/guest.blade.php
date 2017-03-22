1c1c013d5c9085cfc13c26de6b81930d6ca91bf4
Bart Franse <fran0124@hz.nl>
2017-03-22 15:54:05 +0100

    FAQ Systeem

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,3 +1,4 @@
+@include('layouts.help')
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -57,6 +58,7 @@
         </div>
 
         <ul class="nav navbar-top-links navbar-right">
+            <li data-toggle="modal" data-target="#help_guest"><a href="#"><i class="fa fa-question-circle" aria-hidden="true"></i> Help</a></li>
             <li><a href="{{ url('/login') }}"><i class="fa fa-university fa-2x fa-fw" aria-hidden="true"></i>&nbsp;Bibliothecaris / Bibliothecaresse login</a></li>
             <li><a href="{{ url('/register') }}">Registreren (admin)</a></li>
         </ul>
